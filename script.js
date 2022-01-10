995b51f866f0af04fa295b4764194bcfade28100
Francisco Alegria <44007763+mralegria31@users.noreply.github.com>
2022-01-09 23:31:21 -0500

    js edit

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 // Assignment code here
-var alpha = "abcdefghijklmnopqrstuvwxyz";
-var upperAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var numbers = "0123456789" ;
-var symbols = "#$%&'()!+,-./:;<=>?@[]^_`{|}~";
+
+
+
+
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+var alpha = "abcdefghijklmnopqrstuvwxyz";
+var upperAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numbers = "0123456789" ;
+var symbols = "#$%&'()!+,-./:;<=>?@[]^_`{|}~";
+var selectUpperAlpha;
+var selectNumber;
+var selectSymbols;
+
+function generatePasswordLength(){
+  passwordlength = prompt("create a password between 8 to 128 character length") ;
+  if (passwordlength < 8 || passwordlength > 128) {
+    alert("Password must be less than 128")
+  }
+  
+  generatePasswordLength(); 
+
+
+}
 
 // Write password to the #password input
 function writePassword() {
